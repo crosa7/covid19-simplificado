b942936e9dcbdbd0d1650ff1b70498a82ee838fb
gitcrosa <crosa7000@gmail.com>
2021-02-27 11:32:23 +0000

    Added difference from previous day and selection of county

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -12,47 +12,51 @@
             icon="fa fa-user-plus"
             text="Novos casos"
             :cases="numberWithCommas(globalState.confirmados_novos)"
+            :differ="getDifferenceString(previousGlobalState.confirmados_novos, globalState.confirmados_novos)"
         ></card>
         <card
             class="bg-purple-400"
             icon="fa fa-bullseye"
             text="Casos ativos"
             :cases="numberWithCommas(globalState.ativos)"
-        ></card>
+            :differ="getDifferenceString(previousGlobalState.ativos, globalState.ativos)">
+        </card>
         <card
             class="bg-blue-400"
             icon="fa fa-globe"
             text="Casos totais"
-            :cases="numberWithCommas(globalState.confirmados)">
-
+            :cases="numberWithCommas(globalState.confirmados)"
+        >
         </card>
         <card
             class="bg-green-400"
             icon="fa fa-check-circle"
             text="Recuperados totais"
-            :cases="numberWithCommas(globalState.recuperados)">
-
+            :cases="numberWithCommas(globalState.recuperados)"
+            :differ="getDifferenceString(previousGlobalState.recuperados, globalState.recuperados)"
+            invert-diff-color
+        >
         </card>
         <card
             class="bg-pink-400"
             icon="fa fa-user-times"
             text="Óbitos totais"
-            :cases="numberWithCommas(globalState.obitos)">
-
+            :cases="numberWithCommas(globalState.obitos)"
+            :differ="getDifferenceString(previousGlobalState.obitos, globalState.obitos)">
         </card>
         <card
             class="bg-yellow-600"
             icon="fa fa-bed"
             text="Internados"
-            :cases="numberWithCommas(globalState.internados)">
-
+            :cases="numberWithCommas(globalState.internados)"
+            :differ="getDifferenceString(previousGlobalState.internados, globalState.internados)">
         </card>
         <card
             class="bg-orange-500"
             icon="fas fa-procedures"
             text="Internados UCI"
-            :cases="numberWithCommas(globalState.internados_uci)">
-
+            :cases="numberWithCommas(globalState.internados_uci)"
+            :differ="getDifferenceString(previousGlobalState.internados_uci, globalState.internados_uci)">
         </card>
         <div class="md:w-3/4 rounded-2xl bg-yellow-500 text-white py-6 px-4 mr-2 shadow mt-10">
           <div class="text-xs text-gray-200">Última atualização: {{ selectedCountyState.data }}</div>
@@ -87,6 +91,7 @@
         <div
             v-for="countyData in lastUpdatedCountyDataSubSet"
             class="flex justify-between text-xl p-4 cursor-pointer hover:bg-yellow-500 border border-white rounded-2xl"
+            :class="{'bg-yellow-500': selectedCountyState.concelho === countyData.concelho}"
             @click="getStateByCounty(countyData.concelho)"
         >
           <div>
@@ -105,6 +110,7 @@
 import {Component, Vue} from 'vue-property-decorator';
 import axios from 'axios';
 import Card from '@/components/partials/Card.vue';
+import moment from 'moment';
 
 @Component(
     {
@@ -121,6 +127,8 @@ export default class HomePage extends Vue {
 
   protected globalState: any = {};
 
+  protected previousGlobalState: any = {};
+
   protected lastUpdatedCountyData: any = [];
 
   protected lastUpdatedCountyDataSubSet: any = [];
@@ -134,6 +142,29 @@ export default class HomePage extends Vue {
   protected async getGlobalState(): Promise<any> {
     const response = await axios.get('https://covid19-api.vost.pt/Requests/get_last_update');
     this.globalState = response.data;
+  }
+
+  protected async getPreviousGlobalState(): Promise<any> {
+    let yesterday = moment().subtract(1, 'days').format('DD-MM-Y');
+    if (this.globalState.data === yesterday) {
+      yesterday = moment().subtract(2, 'days').format('DD-MM-Y');
+    }
+    const response = await axios.get(`https://covid19-api.vost.pt/Requests/get_entry/${yesterday}`);
+
+    const responseData = response.data;
+    this.previousGlobalState = {
+      confirmados_novos: Object.values(responseData.confirmados_novos)[0],
+      ativos: Object.values(responseData.ativos)[0],
+      confirmados: Object.values(responseData.confirmados)[0],
+      recuperados: Object.values(responseData.recuperados)[0],
+      obitos: Object.values(responseData.obitos)[0],
+      internados: Object.values(responseData.internados)[0],
+      internados_uci: Object.values(responseData.internados_uci)[0],
+    };
+  }
+
+  protected getDifferenceString(oldValue: number, newValue: number): number {
+    return oldValue - newValue;
   }
 
   protected async getStateByCounty(county: string): Promise<any> {
@@ -159,7 +190,8 @@ export default class HomePage extends Vue {
   }
 
   protected async mounted(): Promise<any> {
-    this.getGlobalState();
+    await this.getGlobalState();
+    await this.getPreviousGlobalState();
     await this.getLastUpdateCountyDataSet();
     this.selectedCountyState = this.lastUpdatedCountyData[0];
   }
--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -2,7 +2,23 @@
   <div class="md:w-1/4 rounded-2xl text-white text-xl px-4 py-2 md:py-6 md:px-4 md:mr-4 mb-4 shadow">
     <i :class="[icon]" class="w-1/5"></i>
     <div class="text-xs text-gray-300">{{ text }}</div>
-    <div class="font-bold">{{ cases }}</div>
+    <div class="font-bold">
+      {{ cases }}
+      <span
+          v-if="differ && differ > 0"
+          class="text-2xl"
+          :class="[invertDiffColor ? 'text-red-200':'text-green-200']"
+      >
+        -{{ absoluteDifference }}
+      </span>
+      <span
+          v-else-if="differ"
+          class="text-2xl"
+          :class="[invertDiffColor ? 'text-green-200':'text-red-200']"
+      >
+        +{{ absoluteDifference }}
+      </span>
+    </div>
   </div>
 </template>
 
@@ -19,5 +35,15 @@ export default class Card extends Vue {
 
   @Prop({ type: String, default: '' })
   protected cases!: string;
+
+  @Prop({ type: Number })
+  protected differ!: number;
+
+  @Prop({ type: Boolean })
+  protected invertDiffColor!: boolean;
+
+  get absoluteDifference() {
+    return Math.abs(this.differ);
+  }
 }
 </script>
